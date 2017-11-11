5626fbe98f9942b1a0adf65008aacac7c1d5dcf7
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-11-11 11:48:57 +0200

    8 days

--- src/demo.js
+++ src/demo.js
@@ -95,7 +95,7 @@ export const retrieveEventsForPageType = (type = 'home') => new Promise((resolve
                 dateRanges: [{
                     // startDate: dateToAnalyticsFormat(dateDaysAgo(-1 * daysAgo)),
                     // endDate: dateToAnalyticsFormat(dateDaysAgo(-1 * daysAgo))
-                    startDate: dateToAnalyticsFormat(dateDaysAgo(-2)),
+                    startDate: dateToAnalyticsFormat(dateDaysAgo(-8)),
                     endDate: dateToAnalyticsFormat(dateDaysAgo(0))
                 }],
                 metrics: [{
