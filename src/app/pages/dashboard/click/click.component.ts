a56309cb3fa9ee13f2c4438ac222b14e3935095e
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2018-02-04 10:23:29 +0200

    click info

--- src/app/pages/dashboard/click/click.component.ts
+++ src/app/pages/dashboard/click/click.component.ts
@@ -1,11 +1,23 @@
-import {Component, Input} from '@angular/core';
-import {ClickMessage} from '../dashboard.component';
+import {Component, Input, OnChanges} from '@angular/core';
+import {ClickMessage, ReportRow} from '../dashboard.component';
 
 @Component({
   selector: 'app-click',
   templateUrl: './click.component.html',
   styleUrls: ['./click.component.css']
 })
-export class ClickComponent {
+export class ClickComponent implements OnChanges {
+  @Input() public data: ReportRow[];
   @Input() public click: ClickMessage;
+
+  public users = 0;
+  public clicks = 0;
+
+  public ngOnChanges() {
+    const data = this.data
+      .filter(item => this.click.ea.indexOf(item.eventAction) === 0);
+
+    this.users = Array.from(new Set(data.map(d => d.clientId))).length;
+    this.clicks = data.filter(item => item.eventAction !== 'view').length;
+  }
 }
